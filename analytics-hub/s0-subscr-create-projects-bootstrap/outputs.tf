43e43ed21aee414c340bb95d0f599cf02600672e
Balazs Pinter <predy@google.com>
2024-05-23 13:24:47 +0200

    stage0: add export to outputs

--- analytics-hub/s0-subscr-create-projects-bootstrap/outputs.tf
+++ analytics-hub/s0-subscr-create-projects-bootstrap/outputs.tf
@@ -1,9 +1,9 @@
-output "GOOGLE_BACKEND_IMPERSONATE_SERVICE_ACCOUNT" {
-  description = "GOOGLE_BACKEND_IMPERSONATE_SERVICE_ACCOUNT"
-  value       = google_service_account.terraform_sa.email
+output "cmd1" {
+  description = "cmd1"
+  value       = "export GOOGLE_BACKEND_IMPERSONATE_SERVICE_ACCOUNT=${google_service_account.terraform_sa.email}"
 }
 
-output "GOOGLE_IMPERSONATE_SERVICE_ACCOUNT" {
-  description = "GOOGLE_IMPERSONATE_SERVICE_ACCOUNT"
-  value       = google_service_account.terraform_sa.email
+output "cmd2" {
+  description = "cmd2"
+  value       = "export GOOGLE_IMPERSONATE_SERVICE_ACCOUNT=${google_service_account.terraform_sa.email}"
 }
